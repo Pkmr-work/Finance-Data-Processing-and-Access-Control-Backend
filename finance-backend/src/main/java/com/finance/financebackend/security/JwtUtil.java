ed14f47319c87349f885ef27350523d802cb111a
pushpender <pushpender.working@gmail.com>
2026-04-05 21:49:58 +0530

    Added JWT security in project

---
+++ finance-backend/src/main/java/com/finance/financebackend/security/JwtUtil.java
@@ -0,0 +1,35 @@
+package com.finance.financebackend.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET = "mysecretkeymysecretkeymysecretkey";
+    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateToken(String username) {
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+
+        return Jwts.parser()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
